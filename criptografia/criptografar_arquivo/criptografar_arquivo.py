34a18492d6d1064c046436ca6d22b53c988a8236
VictorGM01 <victormarques8801@gmail.com>
2021-10-03 16:44:15 -0300

    criptografa arquivo

--- criptografia/criptografar_arquivo/criptografar_arquivo.py
+++ criptografia/criptografar_arquivo/criptografar_arquivo.py
@@ -1,8 +1,25 @@
 from cryptography.fernet import Fernet
 
-# chave simétrica
+
 chave = Fernet.generate_key()
 
 # guarda chave em um arquivo
 with open("chave_simetrica.txt", "wb") as arquivo_chave:
     arquivo_chave.write(chave)
+
+# abre o arquivo da chave
+with open("chave_simetrica.txt", "rb") as arquivo_chave:
+    chave_simetrica = arquivo_chave.read()
+
+fernet = Fernet(chave_simetrica)
+
+# abre arquivo teste
+with open("teste.txt", "rb") as arquivo:
+    arquivo_original = arquivo.read()
+
+# criptografa arquivo
+arquivo_criptografado = fernet.encrypt(arquivo_original)
+
+# salva criptografia
+with open("teste.txt", "wb") as criptografado:
+    criptografado.write(arquivo_criptografado)
