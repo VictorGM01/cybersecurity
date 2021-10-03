dbc2b0cf3a61c9a618aaf4ac13b9c2d4d61bd38a
VictorGM01 <victormarques8801@gmail.com>
2021-10-03 16:31:54 -0300

    cria chave simétrica

--- criptografia/criptografar_arquivo/criptografar_arquivo.py
+++ criptografia/criptografar_arquivo/criptografar_arquivo.py
@@ -1,4 +1,8 @@
 from cryptography.fernet import Fernet
 
-
+# chave simétrica
 chave = Fernet.generate_key()
+
+# guarda chave em um arquivo
+with open("chave_simetrica.txt", "wb") as arquivo_chave:
+    arquivo_chave.write(chave)
