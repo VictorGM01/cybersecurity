88f2ba29334005178e6f2d150a54cbeee041a8f6
VictorGM01 <victormarques8801@gmail.com>
2021-10-03 21:10:46 -0300

    cria função para senhas fortes

--- senha/gerador_de_senha/gerador_de_senha.py
+++ senha/gerador_de_senha/gerador_de_senha.py
@@ -2,7 +2,20 @@ import random
 import string
 
 
-def gera_senha_minuscula():
+print("==========gerador de senha==========")
+print("Tipos:\n"
+      "1) Letras minúsculas\n"
+      "2) Letras maiúsculas\n"
+      "3) Caracteres especiais\n"
+      "4) Dígitos\n"
+      "5) Todos os anteriores (recomendado)\n")
+
+
+tipo_de_senha = int(input("Digite o tipo de senha escolhido: "))
+tamanho_da_senha = int(input("Digite o tamanho desejado para a senha: "))
+
+
+def gera_senha_minuscula() -> None:
     caracteres_minusculos = string.ascii_lowercase
     print("==============senha=============\n"
           "Copie sua senha: " +
@@ -34,16 +47,14 @@ def gera_senha_caracteres_especiais():
                   for caractere in range(tamanho_da_senha)))
 
 
-print("==========gerador de senha==========")
-print("Tipos:\n"
-      "1) Letras minúsculas\n"
-      "2) Letras maiúsculas\n"
-      "3) Caracteres especiais\n"
-      "4) Dígitos\n"
-      "5) Todos os anteriores (recomendado)\n")
+def gera_senha_forte():
+    caracteres = string.ascii_letters + string.digits +\
+                 "@#!&$#%*/ç.;:?¨()[]{}|,^~+-¨'_=°ªº"
+    print("==============senha=============\n"
+          "Copie sua senha: " +
+          "".join(random.choice(caracteres)
+                  for caractere in range(tamanho_da_senha)))
 
-tipo_de_senha = int(input("Digite o tipo de senha escolhido: "))
-tamanho_da_senha = int(input("Digite o tamanho desejado para a senha: "))
 
 if tipo_de_senha == 1:
     gera_senha_minuscula()
@@ -56,3 +67,9 @@ elif tipo_de_senha == 3:
 
 elif tipo_de_senha == 4:
     gera_senha_numerica()
+
+elif tipo_de_senha == 5:
+    gera_senha_forte()
+
+else:
+    print("Tipo de senha inválido, tente novamente...")
