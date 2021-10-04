15efbbec8c62920b82dcd83862ac5af0645296cd
VictorGM01 <victormarques8801@gmail.com>
2021-10-03 21:03:23 -0300

    cria funções para senhas numéricas e especiais

--- senha/gerador_de_senha/gerador_de_senha.py
+++ senha/gerador_de_senha/gerador_de_senha.py
@@ -3,10 +3,34 @@ import string
 
 
 def gera_senha_minuscula():
-    caracteres = string.ascii_lowercase
+    caracteres_minusculos = string.ascii_lowercase
     print("==============senha=============\n"
           "Copie sua senha: " +
-          "".join(random.choice(caracteres)
+          "".join(random.choice(caracteres_minusculos)
+                  for caractere in range(tamanho_da_senha)))
+
+
+def gera_senha_maiuscula():
+    caracteres_maiusculos = string.ascii_uppercase
+    print("==============senha=============\n"
+          "Copie sua senha: " +
+          "".join(random.choice(caracteres_maiusculos)
+                  for caractere in range(tamanho_da_senha)))
+
+
+def gera_senha_numerica():
+    caracteres_numericos = string.digits
+    print("==============senha=============\n"
+          "Copie sua senha: " +
+          "".join(random.choice(caracteres_numericos)
+                  for caractere in range(tamanho_da_senha)))
+
+
+def gera_senha_caracteres_especiais():
+    caracteres_especiais = "@#!&$#%*/ç.;:?¨()[]{}|,^~+-¨'_=°ªº"
+    print("==============senha=============\n"
+          "Copie sua senha: " +
+          "".join(random.choice(caracteres_especiais)
                   for caractere in range(tamanho_da_senha)))
 
 
@@ -23,3 +47,12 @@ tamanho_da_senha = int(input("Digite o tamanho desejado para a senha: "))
 
 if tipo_de_senha == 1:
     gera_senha_minuscula()
+
+elif tipo_de_senha == 2:
+    gera_senha_maiuscula()
+
+elif tipo_de_senha == 3:
+    gera_senha_caracteres_especiais()
+
+elif tipo_de_senha == 4:
+    gera_senha_numerica()
